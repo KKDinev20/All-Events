a9a3bba0c0b1729034aaa6f964a0046229f46e26
Konstantin Dinev <kkdinev20@codingburgas.bg>
2024-07-03 14:46:44 +0300

    Add Project Structure

---
+++ AllEvents.TicketManagement/src/UI/AllEvents.TicketManagement.App/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using AllEvents.TicketManagement.App.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AllEvents.TicketManagement.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
