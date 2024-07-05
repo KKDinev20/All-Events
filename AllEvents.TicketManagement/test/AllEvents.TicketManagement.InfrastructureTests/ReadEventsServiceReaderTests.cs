c2a8d6806e42349bf4c87a8df8eb4e55d68d50bc
Konstantin Dinev <kkdinev20@codingburgas.bg>
2024-07-05 12:15:44 +0300

    Fix path in ReadEventsServiceReaderShouldReturnCorrectNumberOfEvents

--- AllEvents.TicketManagement/test/AllEvents.TicketManagement.InfrastructureTests/ReadEventsServiceReaderTests.cs
+++ AllEvents.TicketManagement/test/AllEvents.TicketManagement.InfrastructureTests/ReadEventsServiceReaderTests.cs
@@ -9,7 +9,7 @@ namespace AllEvents.TicketManagement.InfrastructureTests
         public async Task ReadEventsServiceReaderShouldReturnCorrectNumberOfEvents()
         {
             // Arrange
-            var filePath = "..\\..\\..\\..\\..\\..\\AllEvents.TicketManagement\\src\\Infrastructure\\AllEvents.TicketManagement.Persistance\\Data\\EventsData.xlsx";
+            var filePath = "../../../../../../AllEvents.TicketManagement/src/Infrastructure/AllEvents.TicketManagement.Persistance/Data/EventsData.xlsx";
             var reader = new ReadEventsServiceReader();
 
             // Act
