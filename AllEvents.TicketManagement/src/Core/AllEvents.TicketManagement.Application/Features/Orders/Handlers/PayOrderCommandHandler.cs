eb34772661b42667ce51991a083e33983fd4477b
Konstantin Dinev <85335577+KKDinev20@users.noreply.github.com>
2024-08-22 14:17:11 +0300

    Implement pay order option

---
+++ AllEvents.TicketManagement/src/Core/AllEvents.TicketManagement.Application/Features/Orders/Handlers/PayOrderCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using AllEvents.TicketManagement.Application.Contracts;
+using AllEvents.TicketManagement.Application.Features.Orders.Commands;
+using AllEvents.TicketManagement.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace AllEvents.TicketManagement.Application.Features.Orders.Handlers
+{
+    public class PayOrderCommandHandler : IRequestHandler<PayOrderCommand>
+    {
+        private readonly IAllEventsDbContext _context;
+
+        public PayOrderCommandHandler(IAllEventsDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(PayOrderCommand request, CancellationToken cancellationToken)
+        {
+            var order = await _context.Orders
+                .FirstOrDefaultAsync(o => o.Id == request.OrderId, cancellationToken);
+
+            if (order == null)
+            {
+                throw new InvalidOperationException("Order does not exist.");
+            }
+
+            if (order.Status != OrderStatus.Created)
+            {
+                throw new InvalidOperationException("Only orders in 'Created' state can be processed.");
+            }
+
+            order.Status = OrderStatus.Processing;
+
+            _context.Orders.Update(order);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
